0e512f2004a301f316a7e898d539fb2d1e4f2255
TigreDev <donowenrt@gmail.com>
2023-12-29 16:49:38 +0100

    placeholder added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,34 @@ import { ChangeEvent, useState } from "react";
 import { Matchup, getMatchups } from "./utils/tablegenerator";
 import { MatchupTable } from "./MatchupTable";
 
+const placeholderText = `1. Deck and Card Format:
+   - Each matchup starts with the name of the deck.
+   - Cards and amounts should be separated by ": ".
+   - Use "---" to separate different matchups.
+2. Example
+Domain
+Up the beanstalk: -2
+Jace: +2
+---
+Monored
+Knockout blow: +4
+Up the beanstalk: -4
+---
+Esper
+Lithomant Barrage: +2
+Invasion of Zendikar: -2
+`;
+
 function App() {
   const [guideText, setGuideText] = useState("")
   const [guideOutput, setGuideOutput] = useState<Matchup[]>();
   const [colorSchema, setColorSchema] = useState("default");
 
   const handleProcessor = () => {
+    if (guideText === "") {
+      setGuideOutput(undefined)
+      return;
+    }
     const matchups = getMatchups(guideText);
     setGuideOutput(matchups);
   }
@@ -21,7 +43,7 @@ function App() {
       <div className=" text-3xl md:text-6xl text-white ">
         Side Guide Generator
       </div>
-      <textarea className="w-full min-h-32 md:min-h-96 md:w-[550px] px-4 py-2 overflow-y-scroll" value={guideText} onChange={handleChange} />
+      <textarea className="w-full min-h-32 md:min-h-96 md:w-[550px] px-4 py-2 overflow-y-scroll" value={guideText} onChange={handleChange} id='guideText' placeholder={placeholderText} />
       <div className="flex flex-row gap-4">
         <button className="bg-gray-200 rounded-full min-w-10 min-h-10 hover:bg-gray-300 " onClick={() => setColorSchema("grays")} />
         <button className="bg-gradient-to-r from-red-300 to-green-300 rounded-full min-w-10 min-h-10 hover:bg-gradient-to-r hover:from-red-400 hover:to-green-400" onClick={() => setColorSchema("default")} />
