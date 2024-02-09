bb712c19b599b94bb989829ec7b1e60d04b0f09d
TigreDev <donowenrt@gmail.com>
2024-02-09 01:01:48 +0100

    some changes

--- src/components/Home/GeneratorForm.tsx
+++ src/components/Home/GeneratorForm.tsx
@@ -12,13 +12,18 @@ function GeneratorForm() {
         <div className="flex flex-col items-center gap-6 ">
             <SaveDeckForm guide={guideText} />
             <textarea className="w-full min-h-32 md:min-h-96 md:w-[550px] px-2 py-2 overflow-y-scroll rounded-[3px]" value={guideText} onChange={handleChange} id='guideText' placeholder={placeholderText} />
-            <div className="flex flex-row gap-4 ">
-                <button className="bg-gray-200 rounded-full min-w-10 min-h-10 hover:bg-gray-300 " onClick={() => setColorSchema("grays")} />
-                <button className="bg-gradient-to-r from-red-300 to-green-300 rounded-full min-w-10 min-h-10 hover:bg-gradient-to-r hover:from-red-400 hover:to-green-400" onClick={() => setColorSchema("default")} />
-                <Button className="w-20 py-2" onClick={generateOutput}>Generate</Button>
-                <Button className="w-20 py-2" onClick={() => setGuideText("")}>Clear text</Button>
-                <PrintButton className="w-20 py-2" disabled={!guideOutput} />
+            <div className="flex flex-wrap gap-4 justify-center md:justify-start">
+                <div className="flex gap-4 items-center">
+                    <button className="bg-gray-200 rounded-full min-w-10 min-h-10 hover:bg-gray-300" onClick={() => setColorSchema("grays")} />
+                    <button className="bg-gradient-to-r from-red-300 to-green-300 rounded-full min-w-10 min-h-10 hover:bg-gradient-to-r hover:from-red-400 hover:to-green-400" onClick={() => setColorSchema("default")} />
+                </div>
+                <div className="flex gap-4 items-center">
+                    <Button className="w-max md:w-auto px-2 py-2 md:py-2" onClick={generateOutput}>Generate</Button>
+                    <Button className="w-max md:w-auto px-2 py-2 md:py-2" onClick={() => setGuideText("")}>Clear text</Button>
+                    <PrintButton className="w-max md:w-auto px-2 py-2 md:py-2" disabled={!guideOutput} />
+                </div>
             </div>
+
             <div >
                 {guideOutput && <MatchupTable matchups={guideOutput} colorSchema={colorSchema} />}
             </div>
